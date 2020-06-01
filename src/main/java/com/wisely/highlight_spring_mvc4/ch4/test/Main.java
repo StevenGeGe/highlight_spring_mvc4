51103235466b6065cbb172b4a4f89777f8ef8f4c
yong <Yong_GJ@163.com>
2020-06-01 23:06:46 +0800

    [Feature]算法测试函数，两个变量加减

---
+++ src/main/java/com/wisely/highlight_spring_mvc4/ch4/test/Main.java
@@ -0,0 +1,20 @@
+package com.wisely.highlight_spring_mvc4.ch4.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PROJECT_NAME : highlight_spring_mvc4
+ * Version 1.0
+ * Author : yong
+ * package_name : com.wisely.highlight_spring_mvc4.ch4.test
+ * Date :  2020-06-01 22:58
+ * Description:
+ */
+public class Main {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		a += b;
+		System.out.println("a:" + a);
+	}
+}
